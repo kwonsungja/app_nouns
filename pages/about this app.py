b21106491f832119d4f633dccbe85400d1aa7314
kwonsungja <4gg4g4h4@gmail.com>
2024-12-13 20:25:24 +0900

    Create about this app.py

---
+++ pages/about this app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("About This App")
+st.write("To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies.")
